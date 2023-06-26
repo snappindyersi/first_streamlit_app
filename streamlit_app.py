2b578b43465e9ac975342d108485f7c3723141b3
snappindyersi <137764537+snappindyersi@users.noreply.github.com>
2023-06-26 16:12:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 streamlit.title ('My Parents New Healthy Diner')
 
+
 streamlit.header ('Breakfast Menu')
 streamlit.text ('🐔 Chicken')
 streamlit.text ('🥑🍞 Avocado Toast')
+
 
 streamlit.header ('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
